2db82ec359576612d9a3c5d80bd508e33d5c54af
Zhark10 <verygoodman18@yandex.ru>
2020-06-28 12:27:45 +0300

    created navbar, logout

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { useRoutes } from './routes';
 import { useAuth } from './hooks/auth.hook';
 import { AuthContext } from './context/auth.content';
+import { Navbar } from './components/Navbar'
 import 'materialize-css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       token, login, logout, userId, isAuthentificated
     }}>
       <Router>
+        {isAuthentificated && <Navbar/>}
         <div className="container">
           {routes}
         </div>
