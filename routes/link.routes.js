c778657a80682eefab52db67a8d3d6bd88007621
Zhark10 <verygoodman18@yandex.ru>
2020-06-28 13:07:48 +0300

    short id logic

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,25 +1,47 @@
 const {Router} = require('express')
+const config = require('config')
+const shortid = require('shortid')
 const Link = require('../models/Link')
+const auth = require('../middleware/auth.middleware')
 const router = Router()
 
-router.post('/generate', async (req, res) => {
+router.post('/generate', auth, async (req, res) => {
   try {
-  
+    const baseUrl = config.get('baseUrl')
+    const {from} = req.body
+
+    const code = shortid.generate()
+
+    const existing = await Link.findOne({from})
+
+    if (existing) {
+      return res.json({link: existing})
+    }
+
+    const to = baseUrl + '/t/' + code
+    const link = new Link({
+      code, to, from, owner: req.user.userId
+    })
+
+    await link.save()
+
+    res.status(201).json({link})
+    
   } catch (error) {
     res.status(500).json({message: 'Oops... Something went wrong...'})
   }
 })
 
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
   try {
-    const links = await Link.find({owner: null}) // ??
+    const links = await Link.find({owner: req.user.userId}) 
     res.json(links)
   } catch (error) {
     res.status(500).json({message: 'Oops... Something went wrong...'})
   }
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', auth, async (req, res) => {
   try {
     const link = await Link.findById(req,params.id)
     res.json(link)
